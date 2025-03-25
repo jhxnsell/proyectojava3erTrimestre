c33dfed1d92e7a9b87e8f47be9dcfe224d7e2a2a
R4smu <rasmussenmarcoslucas@gmail.com>
2025-03-25 16:52:53 +0100

    mejora menu

---
+++ Menu.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+public class Menu{
+    public static void main (String[] args){ 
+        Scanner sc = new Scanner(System.in);
+        int opcion;
+
+        do {
+            try {
+                System.out.println("1. Seleccionar carpeta");
+                System.out.println("2. Lectura de fichero");
+                System.out.println("3. Convertir");
+                System.out.println("4. Salir");
+                System.out.println("Elige una opcion: ");
+                opcion = sc.nextInt();
+
+                switch (opcion) {
+                    case 1:
+                        
+                        break;
+    
+                    case 2:
+                        
+                        break;
+    
+                    case 3:
+                        
+                        break;
+    
+                    case 4:
+                        System.out.println("Saliendo...");
+                        System.exit(0);
+                        break;
+                
+                    default:
+                        System.out.println("Opción no valida, prueba otra vez");
+                        break;
+                }
+            
+            System.out.println("Ruta de la carpeta: " /*+ rutaCarpeta*/);
+            System.out.println("Contenido de la carpeta: ");
+            System.out.println(/*+ contenidoCarpeta*/);
+            System.out.println("Fichero seleccionado: " /*+ fichero*/);
+                
+            } catch (Exception e) {
+                System.out.println("Error: " + e.getMessage());
+                sc.next();
+            }
+        } while (true);
+    }
+}
