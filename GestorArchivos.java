9490c60a99cff32d86c78e5b0ae64cf2d3123455
R4smu <rasmussenmarcoslucas@gmail.com>
2025-03-26 17:22:04 +0100

    fuuuuuuuuusiooon

--- GestorArchivos.java
+++ GestorArchivos.java
@@ -1,17 +1,4 @@
-import java.io.File;
 import java.util.Scanner;
 public class GestorArchivos{
     Scanner sc = new Scanner(System.in);
-    public void seleccionarArchivo(boolean existe){
-        System.out.println("Introduce la ruta del archivo");
-        String ruta = sc.nextLine();
-        File fichero = new File(ruta);
-            if (fichero.exists()) {
-                existe = true;
-            } else {
-                System.out.println("La ruta seleccionada no existe");
-                existe = false;
-            }
-    }
-
 }
