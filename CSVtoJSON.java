8d9caeb38dfae807b0d5df46a1dd5868ec64ed91
Jhansell <jfgarciav03@iesalbarregas.es>
2025-03-26 17:19:02 +0100

    no funca

--- CSVtoJSON.java
+++ CSVtoJSON.java
@@ -1,24 +1,11 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.Arrays;
-import java.util.List;
+import java.io.*;
+import java.util.*;
 
-public class CSVtoJSON {
-    public static void main(String[] args) throws FileNotFoundException, IOException {
-        String inputFile = "coches.csv";
-        String outputFile = "coches.json";
+public class CSVtoJSON{
+        String CSV;
+        String JSON;
+        
+    public CSVtoJSON(){
 
-        try (BufferedReader br = new BufferedReader(new FileReader(inputFile));
-                FileWriter fileWriter = new FileWriter(outputFile)) {
-                
-                    String linea = br.nextLine();
-                    List<String> headers = Arrays.asList(linea.split(","));
-                    
-                    
-        }
     }
 }
